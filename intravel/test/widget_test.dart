c9f3fa44a1695ab0e0beb8f2ab86be1d08dc26f7
Eunice <eunicekatebelasa1313@gmail.com>
2026-08-10 20:17:02 +0800

    Remove intravel container

--- intravel/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:intravel/main.dart';
-
-void main() {
-  test('has an InTravel app root', () {
-    expect(const InTravelApp(), isA<InTravelApp>());
-  });
-}
